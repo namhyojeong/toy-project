25f759e53f2f453cf7eeab7a789ac9bc485731b4
Hyojeong Nam <namgywjd2@gmail.com>
2023-02-19 22:31:44 +0900

    상점 이용하기에 재료목록리스트 라벨추가

--- src/com/puppy/witchcraft/game/view/StoreMenu.java
+++ src/com/puppy/witchcraft/game/view/StoreMenu.java
@@ -1,11 +1,16 @@
 package com.puppy.witchcraft.game.view;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.puppy.witchcraft.common.MainFrame;
+import com.puppy.witchcraft.game.view.pages.StorePage;
+import static com.puppy.witchcraft.common.CommonConstants.changePanel;
 
 public class StoreMenu extends JPanel {
 
@@ -27,10 +32,29 @@ public class StoreMenu extends JPanel {
 		storeUseBtn.setBounds(330, 175, 450, 100);
 		storeUseBtn.setContentAreaFilled(false);
 		
+		/* 상점이용하기 버튼 클릭 시 상점 화면 패널로 변경 */
+		storeUseBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				changePanel(mf, storeMenu, new StorePage(mf));
+				System.out.println("상점 이용하기 페이지로 이동");
+			}
+		});
 		/* 나가기 버튼 생성 후 상점 나가기 이미지 삽입 */
 		JButton quitBtn = new JButton(new ImageIcon("images/select/map_quit.png"));
 		quitBtn.setBounds(330, 275, 450, 100);
 		quitBtn.setContentAreaFilled(false);
+		
+		
+		/* 나가기 버튼 클릭 시 메인맵 패널로 변경*/
+		quitBtn.addActionListener(new ActionListener() {
+	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				changePanel(mf, storeMenu, new MainMap(mf));
+				System.out.println("메인맵 페이지로 이동");
+			}
+		});
 		
 		/* 컴포넌트들 넣을 패널 생성 */
 		this.setLayout(null);
--- src/com/puppy/witchcraft/game/view/pages/StorePage.java
+++ src/com/puppy/witchcraft/game/view/pages/StorePage.java
@@ -1,12 +1,21 @@
 package com.puppy.witchcraft.game.view.pages;
 
+import static com.puppy.witchcraft.common.CommonConstants.changePanel;
+
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import com.puppy.witchcraft.common.MainFrame;
+import com.puppy.witchcraft.game.view.StoreMenu;
 
 public class StorePage extends JPanel {
 	
@@ -26,6 +35,14 @@ public class StorePage extends JPanel {
 		/* 라벨에 상점재료리스트 이미지 삽입 */
 		JLabel storePurchase = new JLabel(new ImageIcon("images/ui/storeitem_list.png"));
 		storePurchase.setBounds(20, 30, 340, 400);
+		JScrollPane scrollpane = new JScrollPane(storePurchase, 340, 400); //뷰,수직, 수평.  (수식이 길기 때문에 가독성을 위해서)
+
+		storePurchase.setPreferredSize(new Dimension(2000,2000));
+	    JScrollPane js = new JScrollPane(storePurchase);
+	    
+	    js.setPreferredSize(new Dimension(200,200));
+	    
+	    JOptionPane.showMessageDialog(null, js);
 		
 		/* 라벨에 재료인벤토리 이미지 삽입 */
 		JLabel storeSell = new JLabel(new ImageIcon("images/ui/inventory_item.png"));
@@ -36,26 +53,52 @@ public class StorePage extends JPanel {
 		gold.setBounds(635, 450, 25, 25);
 		
 		/* 라벨에 골드 텍스트필드 생성 */
-		/* 텍스트 정렬 구글 찾아보기 */
-		/* 재료 창 구글 찾아보기 */
 		JLabel textId = new JLabel("     0G");
 		textId.setBounds(660, 450, 110, 25);
 		textId.setForeground(Color.WHITE);
 		
-		/* 라벨에 나가기 이미지 삽입 */
-		JLabel quit = new JLabel(new ImageIcon("images/ui/button_quit.png"));
-		quit.setBounds(0, 460, 170, 70);
+		
+		/* 라벨에 나가기 버튼 생성 후 나가기이미지 삽입 */
+		JButton quitBtn = new JButton(new ImageIcon("images/ui/button_quit.png"));
+		quitBtn.setBounds(0, 460, 170, 70);
+		quitBtn.setContentAreaFilled(false);
+		
+		/* 나가기 버튼 클릭 시 상점 첫 화면 패널로 변경*/
+		quitBtn.addActionListener(new ActionListener() {
+	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				changePanel(mf, storePage, new StoreMenu(mf));
+				System.out.println("상점 첫 화면 페이지로 이동");
+			}
+		});
+		
+		/* 라벨에 재료 클릭 버튼 생성 후 상점재료목록 이미지 삽입 */
+		JButton listBtn = new JButton(new ImageIcon("images/ui/store_list.png"));
+		listBtn.setBounds(40, 105, 300, 90);
+		listBtn.setContentAreaFilled(false);
+		
+		JButton listBtn1 = new JButton(new ImageIcon("images/ui/store_list.png"));
+		listBtn1.setBounds(40, 210, 300, 90);
+		listBtn1.setContentAreaFilled(false);
+	
+		JButton listBtn2 = new JButton(new ImageIcon("images/ui/store_list.png"));
+		listBtn2.setBounds(40, 315, 300, 90);
+		listBtn2.setContentAreaFilled(false);
 		
 		/* 컴포넌트들 넣을 패널 생성 */
 		this.setLayout(null);
 		this.setBounds(0, 0, 800, 580);
 		
 		/* 패널에 컴포넌트들 삽입 */
+		this.add(listBtn2);
+		this.add(listBtn1);
+		this.add(listBtn);
 		this.add(storePurchase);
 		this.add(storeSell);
 		this.add(gold);
-		this.add(quit);
 		this.add(textId);
+		this.add(quitBtn);
 		this.add(background);
 		
 		/* 프레임에 패널 올리기 */
